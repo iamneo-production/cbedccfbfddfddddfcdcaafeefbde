027af7ce80273cd1519053ac0b722526b4acf02b
Iamneo <techadmin@iamneo.ai>
2023-08-24 04:56:30 +0000

    Backup Commit

--- jspservlet-app-bmi-calculator/src/main/java/controller/CalculateBMI.java
+++ jspservlet-app-bmi-calculator/src/main/java/controller/CalculateBMI.java
@@ -3,4 +3,9 @@ package controller;
 public class CalculateBMI {
 
 	//Enter the code here....
+	public static double calculate(double height,double weight){
+		double meter=(height/100);
+		double meter2=meter*meter;
+		return (weight/meter2);
+	}
 }
